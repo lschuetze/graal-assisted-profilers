ec75313945bde3b1a422030255cb6a27868ef5d4
mur47x111 <mur47x111@gmail.com>
2015-03-14 11:58:39 +0100

    add sampling to allocation profiler

--- AllocationProfiler/src/ch/usi/dag/profiler/allocation/Instrumentation.java
+++ AllocationProfiler/src/ch/usi/dag/profiler/allocation/Instrumentation.java
@@ -1,6 +1,7 @@
 package ch.usi.dag.profiler.allocation;
 
 import ch.usi.dag.disl.annotation.AfterReturning;
+import ch.usi.dag.disl.annotation.ThreadLocal;
 import ch.usi.dag.disl.classcontext.ClassContext;
 import ch.usi.dag.disl.dynamiccontext.DynamicContext;
 import ch.usi.dag.disl.marker.BytecodeMarker;
@@ -9,19 +10,27 @@ import com.oracle.graal.debug.external.CompilerDecision;
 
 public class Instrumentation {
 
-	@AfterReturning(marker = BytecodeMarker.class, args = "new")
-	static void profileAllocation(DynamicContext dc, TypeInsnContext tic, ClassContext cc) {
-		String key = tic.bci();
-		Class<?> type = cc.asClass(tic.getTypeName());
+	@ThreadLocal
+	static int __samplingCounter;
 
-		if (CompilerDecision.isMethodCompiled()) {
-			if (CompilerDecision.isAllocationVirtual()) {
-				Profiler.profileAlloc(key, type, 3);
+	@AfterReturning(marker = BytecodeMarker.class, args = "new")
+	static void profileAllocation(DynamicContext dc, TypeInsnContext tic,
+			ClassContext cc) {
+		if (__samplingCounter-- <= 0) {
+			__samplingCounter = 1000;
+			String key = tic.bci();
+			Class<?> type = cc.asClass(tic.getTypeName());
+
+			if (CompilerDecision.isMethodCompiled()) {
+				if (CompilerDecision.isAllocationVirtual()) {
+					Profiler.profileAlloc(key, type, 3);
+				} else {
+					Profiler.profileAlloc(key, type,
+							CompilerDecision.getAllocationType());
+				}
 			} else {
-				Profiler.profileAlloc(key, type, CompilerDecision.getAllocationType());
+				Profiler.profileAlloc(key, type, 2);
 			}
-		} else {
-			Profiler.profileAlloc(key, type, 2);
 		}
 	}
 
--- AllocationProfiler/src/ch/usi/dag/profiler/allocation/Profiler.java
+++ AllocationProfiler/src/ch/usi/dag/profiler/allocation/Profiler.java
@@ -1,19 +1,12 @@
 package ch.usi.dag.profiler.allocation;
 
-import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.atomic.AtomicInteger;
 
 import ch.usi.dag.profiler.dump.ArchiveDumper;
 import ch.usi.dag.profiler.dump.Dumper;
 import ch.usi.dag.profiler.meta.ConcurrentCounterMap;
-import ch.usi.dag.profiler.util.ObjectSizeEvaluator;
 
 public class Profiler {
-
-	static {
-		Runtime.getRuntime().addShutdownHook(new Thread(Profiler::dumpMeta));
-	}
 
 	public static final ConcurrentCounterMap errorAlloc = new ConcurrentCounterMap();
 	public static final ConcurrentCounterMap itrprAlloc = new ConcurrentCounterMap();
@@ -38,45 +31,27 @@ public class Profiler {
 		}
 	}
 
-	public static void dumpMeta() {
-		try (Dumper dumper = new ArchiveDumper("meta")) {
-			types.entrySet().stream().sorted(Map.Entry.comparingByKey())
-					.forEach(dumper::println);
-		}
-	}
-
-	public static final AtomicInteger counter = new AtomicInteger();
-	public static final int STRIDE = 1000;
-
 	public static void profileAlloc(String key, Class<?> allocType, int type) {
-		int count = counter.get();
-		if (count < STRIDE) {
-			counter.incrementAndGet();
-		} else if (counter.compareAndSet(count, 0)) {
-			ConcurrentCounterMap which = null;
+		ConcurrentCounterMap which = null;
 
-			switch (type) {
-			case 0:
-				which = tlabAlloc;
-				break;
-			case 1:
-				which = heapAlloc;
-				break;
-			case 2:
-				which = itrprAlloc;
-				break;
-			case 3:
-				which = virtAlloc;
-				break;
-			default:
-				which = errorAlloc;
-			}
-
-			which.increment(key);
-
-			types.computeIfAbsent(key,
-					k -> ObjectSizeEvaluator.sizeof(allocType));
+		switch (type) {
+		case 0:
+			which = tlabAlloc;
+			break;
+		case 1:
+			which = heapAlloc;
+			break;
+		case 2:
+			which = itrprAlloc;
+			break;
+		case 3:
+			which = virtAlloc;
+			break;
+		default:
+			which = errorAlloc;
 		}
+
+		which.increment(key);
 	}
 
 }
