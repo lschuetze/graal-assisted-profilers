d37023c623d2f08bb0e18eda00d17c285b8b9a25
mur47x111 <mur47x111@gmail.com>
2015-03-15 15:46:43 +0100

    remove sampling

--- AllocationProfiler/src/ch/usi/dag/profiler/allocation/Instrumentation.java
+++ AllocationProfiler/src/ch/usi/dag/profiler/allocation/Instrumentation.java
@@ -1,7 +1,6 @@
 package ch.usi.dag.profiler.allocation;
 
 import ch.usi.dag.disl.annotation.AfterReturning;
-import ch.usi.dag.disl.annotation.ThreadLocal;
 import ch.usi.dag.disl.classcontext.ClassContext;
 import ch.usi.dag.disl.dynamiccontext.DynamicContext;
 import ch.usi.dag.disl.marker.BytecodeMarker;
@@ -10,27 +9,21 @@ import com.oracle.graal.debug.external.CompilerDecision;
 
 public class Instrumentation {
 
-	@ThreadLocal
-	static int __samplingCounter;
-
 	@AfterReturning(marker = BytecodeMarker.class, args = "new")
 	static void profileAllocation(DynamicContext dc, TypeInsnContext tic,
 			ClassContext cc) {
-		if (__samplingCounter-- <= 0) {
-			__samplingCounter = 1000;
-			String key = tic.bci();
-			Class<?> type = cc.asClass(tic.getTypeName());
+		String key = tic.bci();
+		Class<?> type = cc.asClass(tic.getTypeName());
 
-			if (CompilerDecision.isMethodCompiled()) {
-				if (CompilerDecision.isAllocationVirtual()) {
-					Profiler.profileAlloc(key, type, 3);
-				} else {
-					Profiler.profileAlloc(key, type,
-							CompilerDecision.getAllocationType());
-				}
+		if (CompilerDecision.isMethodCompiled()) {
+			if (CompilerDecision.isAllocationVirtual()) {
+				Profiler.profileAlloc(key, type, 3);
 			} else {
-				Profiler.profileAlloc(key, type, 2);
+				Profiler.profileAlloc(key, type,
+						CompilerDecision.getAllocationType());
 			}
+		} else {
+			Profiler.profileAlloc(key, type, 2);
 		}
 	}
 
