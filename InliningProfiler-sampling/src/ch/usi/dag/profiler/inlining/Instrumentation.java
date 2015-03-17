16c84edef46a7c45d1696ff5a593c6e87bb95fb3
mur47x111 <mur47x111@gmail.com>
2015-03-17 07:49:01 -0600

    revert to sampling

--- InliningProfiler-sampling/src/ch/usi/dag/profiler/inlining/Instrumentation.java
+++ InliningProfiler-sampling/src/ch/usi/dag/profiler/inlining/Instrumentation.java
@@ -1,21 +1,36 @@
 package ch.usi.dag.profiler.inlining;
 
 import ch.usi.dag.disl.annotation.AfterReturning;
+import ch.usi.dag.disl.annotation.ThreadLocal;
+import ch.usi.dag.disl.marker.BodyMarker;
 import ch.usi.dag.disl.marker.BytecodeMarker;
 
 import com.oracle.graal.debug.external.CompilerDecision;
 
 public class Instrumentation {
 
+	@ThreadLocal
+	static int __samplingCounter;
+
+	@ThreadLocal
+	static InliningProfile __samplingProfile;
+
 	@AfterReturning(marker = BytecodeMarker.class, args = "invokevirtual, invokespecial, invokestatic, invokeinterface")
 	public static void profileInvocation(MethodInsnContext mic) {
 		if (CompilerDecision.isMethodCompiled()) {
 			if (!CompilerDecision.isCallsiteInlined()) {
-				Profiler.DCC_COUNTERS[mic.index()]++;
+				Profiler.profileInvocation(mic.bci());
 			}
 		} else {
-			Profiler.INT_COUNTERS[mic.index()]++;
+			// This is for resolving Profiler.class
+			Profiler.empty();
 		}
+	}
+
+	@AfterReturning(marker = BodyMarker.class, scope = "java.lang.Thread.<init>")
+	static void initProfile() {
+		__samplingCounter = 1000;
+		__samplingProfile = null;
 	}
 
 }
--- InliningProfiler-sampling/src/ch/usi/dag/profiler/inlining/Profiler.java
+++ InliningProfiler-sampling/src/ch/usi/dag/profiler/inlining/Profiler.java
@@ -1,34 +1,77 @@
 package ch.usi.dag.profiler.inlining;
 
+import java.util.HashSet;
+import java.util.LinkedList;
+
 import ch.usi.dag.profiler.dump.ArchiveDumper;
 import ch.usi.dag.profiler.dump.Dumper;
+import ch.usi.dag.profiler.meta.ConcurrentCounterMap;
 
 public class Profiler {
 
-	public static final int STRIDE = 1000;
-	public static final int COUNTER_SIZE = 2 << 18;
+	public static final LinkedList<InliningProfile> profiles = new LinkedList<>();
 
-	public static final int[] DCC_COUNTERS = new int[COUNTER_SIZE];
-	public static final int[] INT_COUNTERS = new int[COUNTER_SIZE];
+	public static final ConcurrentCounterMap notInlinedCounters = new ConcurrentCounterMap();
 
 	public static void clearProfile() {
-		for (int i = 0; i < COUNTER_SIZE; i++) {
-			DCC_COUNTERS[i] = 0;
-			INT_COUNTERS[i] = 0;
+		synchronized (profiles) {
+			for (InliningProfile profile : profiles) {
+				profile.clear();
+			}
 		}
 	}
 
 	public static void dumpProfile(String name) {
 		try (Dumper dumper = new ArchiveDumper(name)) {
-			for (int i = 0; i < COUNTER_SIZE; i++) {
-				int v = DCC_COUNTERS[i];
+			synchronized (profiles) {
+				final HashSet<String> keys = new HashSet<>();
 
-				if (v > 0) {
-					dumper.println(i + " " + v);
-					DCC_COUNTERS[i] = 0;
+				for (InliningProfile profile : profiles) {
+					keys.addAll(profile.counter.keySet());
 				}
+
+				keys.stream().sorted().forEach(key -> {
+					int counter = 0;
+
+					for (InliningProfile profile : profiles) {
+						Integer current = profile.counter.get(key);
+
+						if (current != null) {
+							counter += current;
+						}
+					}
+
+					StringBuilder builder = new StringBuilder(key);
+					builder.append(' ');
+					builder.append(counter);
+					dumper.println(builder.toString());
+				});
 			}
 		}
+	}
+
+	public static void profileInvocation(String bci) {
+		Thread current = Thread.currentThread();
+
+		if (current.__samplingCounter-- <= 0) {
+			current.__samplingCounter = 1000;
+			InliningProfile profile = current.__samplingProfile;
+
+			if (profile == null) {
+				profile = new InliningProfile();
+
+				synchronized (profiles) {
+					profiles.add(profile);
+				}
+
+				current.__samplingProfile = profile;
+			}
+
+			profile.increment(bci);
+		}
+	}
+
+	public static void empty() {
 	}
 
 }
